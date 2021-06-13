368dd4f8df0a70bdfda4a68211c553291e890f2e
Frederik <feiten@365farmnet.com>
2021-06-13 11:20:35 +0200

    initial backend commit

---
+++ be/src/test/java/com/frrede/chat/ChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.frrede.chat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
